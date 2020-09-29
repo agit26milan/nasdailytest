0dec5ec6e07f2e11a886e66dec34f03dd5e3eb39
“agita <agitafirstawan26@gmail.com>
2020-09-29 14:18:45 +0700

    add looping news and dependencies

--- src/Containers/Home/index.js
+++ src/Containers/Home/index.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { request } from '../../Utils'
 
 const HomeContainers = () => {
+  const [listStory, setListStory] = useState([])
+  const [page, setPage] = useState(1)
+  const [perPage, setPerPage] = useState(30)
+  const [loading, setLoading] = useState(true)
+  const getListData = () => {
+    const url = '/topstories.json?print=pretty'
+    request(url, 'GET', {}, (response) => {
+      console.log(response)
+      handleLooping(response.data)
+    }, (e) => {
+      console.log(e, 'kuku')
+    })
+  }
+
+  const getDetailStory = (id) => {
+    const url = `/item/${id}.json?print=pretty`
+    return new Promise((resolve, reject) => {
+      request(url, 'GET', {}, (response) => {
+        resolve(response.data)
+      }, (e) => {
+        reject(e)
+      })
+    })
+  }
+
+  useEffect(() => {
+    getListData()
+  }, [])
+
+  const handleLooping = (arr) => {
+    for (var i = 0; i < arr.length; i++) {
+      getDetailStory(arr[i]).then((data) => {
+        setListStory((oldData) => [...oldData, data])
+      })
+    }
+  }
+
   return (
     <div>
-      <p>HAHAHA</p>
+     
     </div>
   )
 }

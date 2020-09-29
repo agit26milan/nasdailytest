780c353dec99c263d131eed3d5d4815c6545d81a
“agita <agitafirstawan26@gmail.com>
2020-09-29 21:15:45 +0700

    add unit test

--- test/Navbar.test.js
+++ test/Navbar.test.js
@@ -1,8 +1,24 @@
 import React from 'react'
-import { shallow } from 'enzyme';
+import { shallow, configure } from 'enzyme'
+import NavbarComponent from '../src/Components/Navbar'
+import { Navbar } from 'react-bootstrap'
+import Adapter from 'enzyme-adapter-react-16'
+import TestRenderer from 'react-test-renderer'
 
-describe('Check Navbar', () => {
-    it('Should haha', () => {
-        console.log('sasa')
-    });
-});
+configure({ adapter: new Adapter() })
+
+describe('Should make snapshot', () => {
+  const wrapper = TestRenderer.create(<NavbarComponent />)
+  it('should same as snapshot', () => {
+    expect(wrapper).toMatchSnapshot()
+  })
+})
+
+describe('Render navbar component', () => {
+  const wrapper = shallow(<NavbarComponent />)
+  it('Should have navbar componet', () => {
+    expect(wrapper.find(Navbar)).toHaveLength(1)
+    expect(wrapper.find(Navbar.Brand)).toHaveLength(1)
+    expect(wrapper.find(Navbar.Toggle)).toHaveLength(1)
+  })
+})
